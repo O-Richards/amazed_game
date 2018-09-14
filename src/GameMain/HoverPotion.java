abd7ecba5e5af4e13cb086ecbe16c75ccbea56e7
O-Richards <oli.richards@gmail.com>
2018-09-14 22:19:20 +1000

    Hover Potion Works

--- src/GameMain/HoverPotion.java
+++ src/GameMain/HoverPotion.java
@@ -11,6 +11,8 @@ public class HoverPotion extends UsableEntity {
 	public Collision collide(MobileEntity hitter) {
 		if (hitter.pickup(this)) {
 			this.removeFromTile();
+			//Applying decorator pattern to the player's movements
+			//i.e. we wrap the player's movemetn with a hover bonus
 			hitter.setMovement(new HoverBonusMovement(hitter.getMovement()));
 		}
 		return Collision.MOVE;		
