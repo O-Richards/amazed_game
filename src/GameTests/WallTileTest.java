3ecc80984910143eed4641b48d20b67b6f0bb429
Geoffrey <z5162782@student.unsw.edu.au>
2018-09-16 23:42:25 +1000

    Wall Tiles added

--- src/GameTests/WallTileTest.java
+++ src/GameTests/WallTileTest.java
@@ -1,34 +1,47 @@
 package GameTests;
 
 import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.Test;
 
-public class WallTileTest {
+import GameMain.Collision;
+import GameMain.Coord;
+import GameMain.DoorTile;
+import GameMain.PlayerMobileEntity;
+import GameMain.WallTile;
+import GameMain.WinSystem;
+import GameMain.WinType;
 
+public class WallTileTest {
+	PlayerMobileEntity p;
+	WallTile w; 
+	WinSystem winSystem;
+	Coord c ;
+	@Test
+	public void initialConditions() {
+		c = new Coord(2, 2);
+		p = new PlayerMobileEntity(c);
+		winSystem = new WinSystem();
+		w = new WallTile(c, this.winSystem.newWinCondition(WinType.WIN));		
+	}
 	@Test
 	public void testCollideExt() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testUpdateWinCondition() {
-		fail("Not yet implemented");
+		initialConditions();
+		assertEquals(w.collideExt(p, Collision.NOMOVE), Collision.NOMOVE);
 	}
 
 	@Test
 	public void testGetSprite() {
-		fail("Not yet implemented");
+		initialConditions();
+		assertEquals(w.getSprite(),"W");
 	}
 
-	@Test
-	public void testWallTile() {
-		fail("Not yet implemented");
-	}
 
 	@Test
 	public void testEqualsObject() {
-		fail("Not yet implemented");
+		initialConditions();
+		assertTrue(w.equals(new WallTile(c, this.winSystem.newWinCondition(WinType.WIN))));
 	}
 
 }
