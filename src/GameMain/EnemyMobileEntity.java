6f3e87467b3f573001646c2d64ab58d35dcfcfae
min-maxwell <z3464892@ad.unsw.edu.au>
2018-09-16 17:28:11 +1000

    hmm

--- src/GameMain/EnemyMobileEntity.java
+++ src/GameMain/EnemyMobileEntity.java
@@ -2,10 +2,15 @@ package GameMain;
 
 public class EnemyMobileEntity extends MobileEntity {
 
+	private Level level;
 	EnemyMobileEntity(Coord coord) {
 		super(coord);
 	}
-	
+	EnemyMobileEntity(Coord coord, Level level) {
+		super(coord);
+		this.level = level;
+
+	}
 	@Override
 	public Collision collide(MobileEntity hitter, boolean recall) {
 		hitter.killPlayer();
@@ -34,5 +39,11 @@ public class EnemyMobileEntity extends MobileEntity {
 	public String getSprite() {
 		return "E";
 	}
-
+	
+	@Override
+	public void move() {
+		Coord nextCoord = this.nextCoord();
+		if (DEBUG) System.out.println("Moving sprite " + this.getSprite() + " to " + nextCoord);
+		this.entityMover.moveEntity(this, nextCoord);
+	}
 }
