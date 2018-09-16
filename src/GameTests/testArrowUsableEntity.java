324c9a7c4c74bd7f5b4e9e88ec52fd4058667267
Alexander <Alexander@10.248.212.53>
2018-09-16 13:20:13 +1000

    added two unimplemented test cases

---
+++ src/GameTests/testArrowUsableEntity.java
@@ -0,0 +1,14 @@
+package GameTests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class testArrowUsableEntity {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/GameTests/testBoulderUsableEntityTest.java
@@ -0,0 +1,14 @@
+package GameTests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class testBoulderUsableEntityTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
