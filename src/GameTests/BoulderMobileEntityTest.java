219da7052535a548766a057a28796dc994c9d751
Alexander <Alexander@10.248.212.53>
2018-09-16 20:33:26 +1000

    Boulder tests

--- src/GameTests/BoulderMobileEntityTest.java
+++ src/GameTests/BoulderMobileEntityTest.java
@@ -2,38 +2,44 @@ package GameTests;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import GameMain.ArrowMobileEntity;
+import GameMain.Coord;
+import GameMain.Direction;
+import GameMain.PlayerMobileEntity;
+import GameMain.UsableEntity;
 
 public class BoulderMobileEntityTest {
 
-	@Test
-	public void testGetSprite() {
-		fail("Not yet implemented");
+	Coord c;
+	Direction d;
+	PlayerMobileEntity p;
+	ArrowMobileEntity a;
+	UsableEntity u;
+	
+	@Before
+	public void setUp() throws Exception {
+		c = new Coord(5, 8);
+		d = Direction.DOWN;
+		p = new PlayerMobileEntity(c);
+		a = new ArrowMobileEntity(c, d);
 	}
-
+	
 	@Test
 	public void testPickup() {
-		fail("Not yet implemented");
+		assertFalse(a.pickup(u));
 	}
 
 	@Test
 	public void testKillPlayer() {
-		fail("Not yet implemented");
+		assertFalse(a.killEnemy());
 	}
 
 	@Test
 	public void testKillEnemy() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testBoulderMobileEntity() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testCollide() {
-		fail("Not yet implemented");
+		assertFalse(a.killPlayer());
 	}
 
 }
