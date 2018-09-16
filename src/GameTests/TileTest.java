f3e7ba36dec056f76cebbd97f0cf5719e6e12478
Oli R <=>
2018-09-16 18:55:46 +1000

    added tile test

--- src/GameTests/TileTest.java
+++ src/GameTests/TileTest.java
@@ -1,39 +1,105 @@
 package GameTests;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Before;
+import org.junit.Test;
 
 import GameMain.Coord;
 import GameMain.EmptyTile;
+import GameMain.Entity;
+import GameMain.ExitTile;
+import GameMain.HoverPotion;
+import GameMain.SwitchTile;
 import GameMain.Tile;
+import GameMain.WallTile;
 import GameMain.WinSystem;
 import GameMain.WinType;
 import junit.framework.TestCase;
 
-class TileTest extends TestCase{
+public class TileTest extends TestCase {
+	List<Tile> tiles;
+	Tile switchTile;
+	Tile exitTile;
+	Tile wallTile;
+	Tile emptyTile;
 
-	@Override
-	protected void setUp() throws Exception {
+	Coord tileCoord;
+	@Before
+	public void setUp() throws Exception {
 		WinSystem winSystem = new WinSystem();
-		Tile emptyTile = new EmptyTile(new Coord(1, 1), winSystem.newWinCondition(WinType.WIN), winSystem.newWinCondition(WinType.WIN));
-		Tile wallTile = new EmptyTile(new Coord(1, 1), winSystem.newWinCondition(WinType.WIN), winSystem.newWinCondition(WinType.WIN));
-		Tile exitTile = new EmptyTile(new Coord(1, 1), winSystem.newWinCondition(WinType.WIN), winSystem.newWinCondition(WinType.WIN));
-		Tile switchTile = new EmptyTile(new Coord(1, 1), winSystem.newWinCondition(WinType.WIN), winSystem.newWinCondition(WinType.WIN));
-		List<Tile> tiles = new ArrayList<>();
+		tileCoord = new Coord(1, 1);
+		emptyTile = new EmptyTile(tileCoord, winSystem.newWinCondition(WinType.WIN), winSystem.newWinCondition(WinType.WIN));
+		wallTile = new WallTile(tileCoord, winSystem.newWinCondition(WinType.WIN));
+		exitTile = new ExitTile(tileCoord, winSystem.newWinCondition(WinType.WIN), winSystem.newWinCondition(WinType.WIN));
+		switchTile = new SwitchTile(tileCoord, winSystem.newWinCondition(WinType.WIN), winSystem.newWinCondition(WinType.WIN));
+		tiles = new ArrayList<>();
 		tiles.add(emptyTile);
 		tiles.add(wallTile);
 		tiles.add(exitTile);
 		tiles.add(switchTile);
-
 	}
-	
+
 	@Test
-	void testAdd() {
-		//test adding
+	public void testTick() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testContainsEntity() {
+		Entity entity = new HoverPotion(tileCoord)l
+		for (Tile t : tiles) {
+			t.addEntity(entity);
+			assertTrue(t.containsEntity(entity));
+		}
+	}
+
+	@Test
+	public void testAddEntity() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemoveEntity() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCollide() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCollideExt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdateEnemyCondition() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdateWinCondition() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetCoord() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetSprite() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testKillEnemyEntities() {
+		fail("Not yet implemented");
 	}
 
 }
