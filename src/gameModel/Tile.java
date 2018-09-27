0337cd81e9081a1cd664e8389a9734aaa54b7988
test <test@192.168.1.102>
2018-09-27 15:56:08 +1000

    tile is refactored but errors and other classes need to be adapted to the changes

--- src/gameModel/Tile.java
+++ src/gameModel/Tile.java
@@ -1,15 +1,11 @@
 package gameModel;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-
-public abstract class Tile implements Collidable {
-	private static final boolean DEBUG = false;
+public abstract class Tile {
 	private Coord coord;
 	private WinCondition enemyCondition;
 	private UsableEntity item = null;
-	private MobileEntity mobileEntity = null;
+	private MobileEntity enemy = null;
+	private PlayerMobileEntity player = null;
 	
 	public Tile(Coord coord, WinCondition enemyCondition) {
 		this.coord = coord;
@@ -17,44 +13,43 @@ public abstract class Tile implements Collidable {
 	}
 	
 	public void tick(int tickNum) {
-		List<Entity> listCopy = new ArrayList<>(this.entities);
-		for (Entity e : listCopy) {
-			e.tick(tickNum);
-		}
+		item.tick(tickNum);
+		enemy.tick(tickNum);
+		player.tick(tickNum);
 	}
 	/**
 	 * Adds a usable item to the tile: 
 	 * @param item
 	 * @throws EntityPlacementException
 	 */
-	public void addUsableEntity(UsableEntity item) throws EntityPlacementException {
+	public void addItem(UsableEntity item) throws EntityPlacementException {
 		if (this.item != null) {
-			throw new EntityPlacementException("Occupied tile");
+			throw new EntityPlacementException("Item on tile");
 		}
 		this.item = item; 
 	}
 	
-	public void addMoveableEntity(MobileEntity entity) throws EntityPlacementException {
-		if(this.mobileEntity != null) {
-			throw new EntityPlacementException("Mobile Entity on tile");
+	public void addEnemy(MobileEntity enemy) throws EntityPlacementException {
+		if(this.enemy != null) {
+			throw new EntityPlacementException("Enemy on tile");
 		}
-		this.mobileEntity = entity; 
+		this.enemy = enemy; 
 	}
 	
-	public MobileEntity getMoveableEntity() {
-		return mobileEntity;
+	public void addPlayer(PlayerMobileEntity player) throws EntityPlacementException {
+		if(this.player != null) {
+			throw new EntityPlacementException("Player on tile");
+		}
+		this.player = player; 
+	}
+	
+	public MobileEntity getEnemy() {
+		return this.enemy;
 		
 	}
 	public Entity getItem() {
-		
+		return this.item;
 	}
-	/**
-	 * @param entity the entity on an adjacent tile to be moved onto this tile
-	 */
-	public void moveMobileEntityOnto(MobileEntity entity) {
-		entity.collide(this, recall)
-	}
-	
 
 	/**
 	 * Collide the hitter with the Tile. MobileHitters have a bunch of methods
@@ -65,52 +60,11 @@ public abstract class Tile implements Collidable {
 	 * @param hitter The mobile entity that is walking into the collidable object
 	 * @return MOVE if the movement is possible, NOMOVE if the movement is blocked
 	 */
-	
-	@Override
-	public Collision collide(MobileEntity hitter, boolean recall) {			
-		Collision col = Collision.MOVE;
-		/*for (Entity e : entities) {
-			//Prevent self collisions
-			if (e != hitter) {
-				Collision tmpCol = e.collide(hitter, recall);
-				if (tmpCol != Collision.MOVE) {
-					col = tmpCol;
-				}
-			}
-		}*/
-		//If two mobile entities hit: 
-		if(this.mobileEntity != hitter) {
-			Collision tmpCol = mobileEntity.collide(hitter, recall);
-			if (tmpCol != Collision.MOVE) {
-				col = tmpCol;
-			}
-		}
-		//If a mobile entity hits an item: 
-		//if a player collides with a usable entity 
-		if(hitter.pickup(item)) {
-			
-		}
-		//enemy calls kill on square when moved
-		 
-		return collideExt(hitter, col);
-	}
-
-	
-	/**
-	 * Allows to extend the regular collide behavior and is implemented by the relevant subclasses
-	 * @param hitter
-	 * @param col
-	 * @return
-	 */
-	public Collision collideExt(MobileEntity hitter, Collision col) {
-		return col;
-	}
-	
 	protected void updateEnemyCondition() {
-		if (this.containsEntity(new EnemyMobileEntity(this.getCoord()))) {
-			this.enemyCondition.setType(WinType.ENEMY);
-		} else {
+		if (this.enemy == null) {
 			this.enemyCondition.setType(WinType.WIN);
+		} else {
+			this.enemyCondition.setType(WinType.ENEMY);
 		}
 	}
 	
@@ -121,12 +75,12 @@ public abstract class Tile implements Collidable {
 	}
 	
 	public String getSprite() {
-		if (this.entities.isEmpty()) {
-			return " ";
+		if (this.enemy != null) {
+			return this.enemy.getSprite();
+		} else if (this.item != null) {
+			return this.item.getSprite();
 		} else {
-			//TODO: Don't ignore entities after the 1st one
-			Iterator<Entity> entityIt = this.entities.iterator();
-			return entityIt.next().getSprite();
+			return " ";
 		}
 	}
 
@@ -135,23 +89,9 @@ public abstract class Tile implements Collidable {
 	 * Use this for bombs, swords etc.
 	 * @Return true if an item is removed from tile false otherwise: 
 	 */
-	public boolean killEnemyEntities() {
-		Iterator<Entity> it = this.entities.iterator();
-		System.out.println(getCoord()+ "Current Coordinates");
-		while (it.hasNext()) {
-			Entity e = it.next();
-			//Removes the enemy: 
-			if (e.equals(new EnemyMobileEntity(this.getCoord()))) {
-				it.remove();
-				return true; 
-			}
-
-		}
-		return false;
+	public void killEnemyEntity() {
+		this.enemy = null;
 	}
-	
-
-	
 }
 
 class EntityPlacementException extends Exception {
