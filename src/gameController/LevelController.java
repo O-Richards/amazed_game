916dbf64d8d28f02cc0dc3d053290e71dab1db6a
Oliver Richards <o.richards@student.unsw.edu.au>
2018-09-27 12:19:27 +1000

    Added controller interface

---
+++ src/gameController/LevelController.java
@@ -0,0 +1,20 @@
+package gameController;
+
+import gameModel.Action;
+import gameModel.Direction;
+
+public interface LevelController {
+	EntityType[][] getMap();
+	int getNumRows();
+	int getNumCols();
+	boolean placeEntity(EntityType entityType, int x, int y);
+	boolean removeEntity(EntityType entityType, int x, int y);
+	boolean clearTile(int x, int y);
+	boolean hasLost();
+	boolean hasWon();
+	void setPlayerDirection(Direction direction);
+	void setPlayerSpeed(int speed);
+	boolean useItem(Action item);
+	
+	
+}
