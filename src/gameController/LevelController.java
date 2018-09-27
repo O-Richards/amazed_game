b28403b5ebcbf904396123d7272d7b4200048711
Oliver Richards <o.richards@student.unsw.edu.au>
2018-09-27 14:54:26 +1000

    changed interface to observer

--- src/gameController/LevelController.java
+++ src/gameController/LevelController.java
@@ -1,14 +1,23 @@
 package gameController;
 
+import java.util.Observer;
+
 import gameModel.Action;
+import gameModel.Coord;
 import gameModel.Direction;
 
 public interface LevelController {
-	EntityType[][] getMap();
 	int getNumRows();
 	int getNumCols();
 	void newMaze(int numRows, int numCols);
-	boolean placeEntity(EntityType entityType, int x, int y);
+	/**
+	 * @param entityGraphic The front-end observer for the new entity
+	 * @param entityType The type of the new entity
+	 * @param x the row to place the new entity on
+	 * @param y the column to place the new entity on
+	 * @return
+	 */
+	boolean placeEntity(Observer entityGraphic, EntityType entityType, int x, int y);
 	boolean removeEntity(EntityType entityType, int x, int y);
 	boolean clearTile(int x, int y);
 	boolean hasLost();
