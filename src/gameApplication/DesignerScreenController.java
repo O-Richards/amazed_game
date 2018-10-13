2efa0055a535f1046f8cb660aa37683a038f113a
Geoffrey <z5162782@student.unsw.edu.au>
2018-10-13 20:04:05 +1100

    added a listner for designerScreen

--- src/gameApplication/DesignerScreenController.java
+++ src/gameApplication/DesignerScreenController.java
@@ -1,20 +1,25 @@
 package gameApplication;
 
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
 
-import org.omg.CORBA.PUBLIC_MEMBER;
+import javafx.beans.Observable;
 
+
+import gameModel.EntityMaker;
 import gameModel.Level;
+import gameModel.entity.VisType;
+import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 
 public class DesignerScreenController {
+	private VisType currentlySelected; 
 	@FXML
 	private Button arrow; 
 	@FXML
@@ -60,137 +65,181 @@ public class DesignerScreenController {
 	@FXML
 	private CheckBox treasureCondition;
 	@FXML
+	private Button clear; 
+
+	@FXML
 	private GridPane map;
-	@FXML
-	private AnchorPane anchorPane;
-	@FXML
-	private ImageView someimage; 
 	
-	private Level aLevel; 
+	private Level l; 
+	private EntityMaker make; 
 	
+	private int xCoordClicked; 
+	private int yCoordClicked; 
+
 	private boolean playerPlaced = false; 
 
 	private static final int DEFAULT_NROWS = 50;
 	private static final int DEFAULT_NCOLS = 50;
-	//Item we have clicked: 
-	//Have an enum of what item we clicked depends on the button: 
-	
-	
-	//Clicked rows and columns: 
-	private int clickedRow = 0; 
-	private int clickedColumn = 0; 
-	
-	//by creates a map of size 50x50
+	//A list observable list: 
+	//updates if there's changes to clicked boolean in JFXPanes 
+	private ObservableList<JFXPane> gridOfPanes = FXCollections.observableArrayList(item -> new Observable[] {item.clickedProperty()}); 
+
 	@FXML
     public void initialize() {
-		aLevel = new Level();
-		//Prevents the user from setting checkboxes unless placed down:
+		//we need to pass a value to the level constructor for the proper size: 
+		l = new Level();
+		make = new EntityMaker(l.getWinSystem(), l.getEntityMover());
+		//Prevents the user from setting checkboxes unless corresponding items are placed down:
 		exitCondition.setDisable(true);
 		enemyCondition.setDisable(true);
 		switchCondition.setDisable(true);
 		treasureCondition.setDisable(true);
-		//Note this uses the no args constructor.. 
+		
 		//creates a map which has a size of default row and col (we will change this) 
-		//will be moving this into an update fuction: 
+		//will be moving this into an update function: 
 		for (int row = 0; row < 15; row++) {
 			for (int col = 0; col < 15; col++) {
-				//adds an image view to the map: 
-				ImageView aNewIMage = new ImageView(new Image(getClass().getResourceAsStream("/wall.png"),20,20,true,true));
-				map.add(aNewIMage, col, row);
-				//Adds a pane which listens for mouse clicks
-				addPane(row, col);
+				//Creates a JFXPane: 
+				JFXPane aPane = new JFXPane(row,col);
+				//Tell the JFXPane to detect Mouse clicks: 
+				aPane.detectMouseClicks();
+				//Adds the pane to our gridView: 
+				map.add(aPane.getPane(), col, row);
+				//Attach it to our observable list: 
+				gridOfPanes.add(aPane);
 			} 	
 		}
+		//Listens for changes in our observableList: 
+		gridOfPanes.addListener((ListChangeListener<JFXPane>) change -> {
+			while(change.next()) {
+				if(change.wasUpdated()) {
+					int elementChanged = change.getFrom(); 	
+					System.out.println(gridOfPanes.get(elementChanged).getRow()); 
+					System.out.println(gridOfPanes.get(elementChanged).getColumn()); 
+					gridOfPanes.get(elementChanged).resetClicked();
+				}
+			}
+		});
+		
+	}
+
+	@FXML	
+	public void setPlayer() {
+		selectedItem.setText("Player");
+		//If player has already been placed:
+		//Maybe frontend shouldn't implement this? 
+		if(playerPlaced) {
+			Alert alert = new Alert(Alert.AlertType.WARNING);
+			alert.getDialogPane().setContent(new Text("A player has already been placed on the map"));
+			alert.showAndWait();
+			selectedItem.setText("-");
+		}else{
+			currentlySelected = VisType.PLAYER;
+		}
+
+	}
+	@FXML
+	public void setExit() {
+		selectedItem.setText("exit");
+		//Enables the exit checkbox: 
+		exitCondition.setDisable(false);
+		currentlySelected = VisType.EXIT;
+	}
+	@FXML
+	public void setSwitch() {
+		selectedItem.setText("switch");
+		//Enables switch win condition:
+		switchCondition.setDisable(false);
+		currentlySelected = VisType.SWITCH; 
+	}
+	@FXML
+	public void setTreasure() {
+		selectedItem.setText("treasure");
+		//Enables treasure win condition:
+		treasureCondition.setDisable(false);
+		currentlySelected = VisType.TREASURE; 
+	}
+	@FXML
+	public void setWall() {
+		selectedItem.setText("Wall");
+		currentlySelected = VisType.WALL; 
+	}
+	@FXML
+	public void setDoor() {
+		selectedItem.setText("door");
+		currentlySelected = VisType.DOOR; 
+	}
+
+	@FXML
+	public void setPit() {
+		selectedItem.setText("Pit");
+		currentlySelected = VisType.PIT;
+	}
+	
+	@FXML
+	public void setBoulder() {
+		selectedItem.setText("Boulder");
+		currentlySelected = VisType.BOULDER;
+	}
+
+	@FXML
+	public void setKey() {
+		selectedItem.setText("Key");
+		currentlySelected = VisType.KEY; 
+	}
+	@FXML
+	public void setArrow() {
+		selectedItem.setText("Arrow");
+		currentlySelected = VisType.ARROW;
+	}
+	@FXML
+	public void setSword() {
+		selectedItem.setText("Sword");
+		currentlySelected = VisType.SWORD;
+	}
+	@FXML
+	public void setBomb() {
+		selectedItem.setText("Bomb");
+		currentlySelected = VisType.BOMB;
+
+	}
+	@FXML
+	public void setHound() {
+		selectedItem.setText("Hound");
+		//TODO: NEED TO CHANGE THIS!!!
+		currentlySelected = VisType.HUNTER;
+
+	}
+	@FXML
+	public void setStrat() {
+		selectedItem.setText("Strategiest");
+		//TODO: CHANGE THIS: 
+		currentlySelected = VisType.HUNTER;
+	}
+	@FXML
+	public void setCoward() {
+		selectedItem.setText("Coward");
+		//TODO: CHANGE THIS: 
+		currentlySelected = VisType.HUNTER;
+	}
+	@FXML
+	public void setHoverPotion() {
+		selectedItem.setText("Hover Potion");
+		currentlySelected = VisType.HOVER_POTION;
+	}
+	@FXML
+	public void setInvincibilityPotion() {
+		selectedItem.setText("Inviniciblity Potion");
+		currentlySelected = VisType.INVINCIBILITY_POTION; 
+	}
+	@FXML 
+	public void clearSelected() {
+		selectedItem.setText("-");
+		currentlySelected = null; 
 	}
 	@FXML
 	public void setItem() {
 		
 	}
-	//Adds a pane to every single grid we make so that we can listen for a mouse click event:  
-	private void addPane(int row, int col) {
-		Pane pane = new Pane(); 
-		//Listens for a mouse click event: 
-		//Sets the clicked rows and columns: 
-		pane.setOnMouseClicked(e-> {
-            clickedRow = row;
-            clickedColumn = col;   
-            
-        });
-		
-		
-        map.add(pane, col, row);		
-	}
-	
-	
-	// ALL the buttons: 	
-	@FXML	
-	public void setPlayer() {
-		//if(!playerPlaced) {set the current enum to player: and set label}
-	}
-	@FXML
-	public void setWall() {
-		//Sets the wall down 
-	}
-	@FXML
-	public void setDoor() {
-		
-	}
-	@FXML
-	public void setExit() {
-		//Enables the exit checkbox: 
-		exitCondition.setDisable(false);
-	}
-	@FXML
-	public void setPit() {
 
-	}
-	@FXML
-	public void setSwitch() {
-		//Enables switch win condition:
-		switchCondition.setDisable(false);
-	}
-	@FXML
-	public void setBoulder() {
-		
-	}
-	@FXML
-	public void setTreasure() {
-		//Enables treasure win condition:
-		treasureCondition.setDisable(false);
-	}
-	@FXML
-	public void setKey() {
-	}
-	@FXML
-	public void setArrow() {
-		
-	}
-	@FXML
-	public void setSword() {
-		
-	}
-	@FXML
-	public void setBomb() {
-		
-	}
-	@FXML
-	public void setHound() {
-		
-	}
-	@FXML
-	public void setStrat() {
-		
-	}
-	@FXML
-	public void setCoward() {
-		
-	}
-	@FXML
-	public void setHoverPotion() {
-		
-	}
-	@FXML
-	public void setInvincibilityPotion() {
-		
-	}
 }
--- src/gameApplication/JFXPane.java
+++ src/gameApplication/JFXPane.java
@@ -1,22 +1,60 @@
 package gameApplication;
 import java.util.Observable;
 import java.util.Observer;
-import gameModel.tile.Tile;
+
+import gameModel.entity.VisType;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 
 public class JFXPane implements Observer {
 	
-
 	private Pane pane;
-	private Image image; // TODO <<== enum fed from Tile based upon movable object 	
-			
-	public JFXPane() {
-		// TODO Auto-generated method stub
+	private ImageView image; 
+	private int row; 
+	private int column; 
+	private SimpleBooleanProperty clicked; 
+
+	public JFXPane(int row, int column) {
+		this.row = row; 
+		this.column = column; 
+		clicked = new SimpleBooleanProperty(false); 
+		//Defaults to a set of tiles: 
+		this.image = new ImageView(new Image(getClass().getResourceAsStream("/tile.png"),20,20,true,true));			
+		pane = new Pane(image);
 	}
-	
+	//Passes back the pane to javafx: 
+	public Pane getPane() {
+		return pane;
+	}
+	//Listens for mouse clicks: 
+	public void detectMouseClicks() {
+		pane.setOnMousePressed(e-> {
+			System.out.println("Clicked on "+ row + " "+column);
+			clicked.set(true);
+        });
+		
+	}
+
 	@Override
 	public void update(Observable o, Object arg) {
-		// TODO Auto-generated method stub
+		//changes the images: 
+		//not sure what observable o does and object arg does?? 
+			
+	}
+	public int getRow() {
+		return this.row; 
+	}
+	public int getColumn() {
+		return this.column; 
+	}
+	public BooleanProperty clickedProperty() {
+		return clicked; 
+	}
+	public void resetClicked() {
+		clicked.set(false);
 	}
 
 }
